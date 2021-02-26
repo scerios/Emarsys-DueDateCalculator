f1943897b28ec369f30ccb9e7ffa5b4f0cde010f
Takacs Robert <89.t.robert@gmail.com>
2021-02-26 10:47:36 +0100

    feat(): added test for GetHowManyDays() / Hours()

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -3,6 +3,14 @@ import { Calculator } from '../app/Calculator';
 const date = new Date(2021, 1, 22, 16, 12);
 const calculator = new Calculator(date, 6);
 
+test('GetHowManyDays days should be 0', () => {
+	expect(calculator.GetHowManyDays(6)).toBe(0);
+});
+
+test('GetHowManyHours hours should be 6', () => {
+	expect(calculator.GetHowManyHours(6)).toBe(6);
+});
+
 test('CalculateDueHour issue hours should be 14', () => {
 	expect(calculator.CalculateDueHour(calculator.issueDue)).toEqual({
 		day: 1,
