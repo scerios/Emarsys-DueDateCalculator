652eec7d26cc4e68648742b591ff9d609455126e
Takacs Robert <89.t.robert@gmail.com>
2021-02-26 10:24:01 +0100

    feat(): added GetHowManyDays() / Hours()

--- src/app/Calculator.js
+++ src/app/Calculator.js
@@ -20,6 +20,22 @@ export class Calculator {
         this.issueDue.howManyHours = this.GetHowManyHours(this.turnaround);
     }
 
+    GetHowManyDays(turnaround) {
+        if (!Number.isInteger(turnaround) || turnaround < 0) {
+            throw new Error("Invalid input number. Input must be an integer greater than or equal to 0.");
+        }
+
+        return parseInt(turnaround / 8);
+    }
+
+    GetHowManyHours(turnaround) {
+        if (!Number.isInteger(turnaround) || turnaround < 0) {
+            throw new Error("Invalid input number. Input must be an integer greater than or equal to 0.");
+        }
+
+        return turnaround % 8;
+    }
+
     CalculateDueHour(issueDue) {
         for (let i = 0; i < issueDue.howManyHours; i++) {
             if (!this.workHours.includes(issueDue.hours + 1)) {
@@ -32,3 +48,4 @@ export class Calculator {
 
         return issueDue;
     }
+}
