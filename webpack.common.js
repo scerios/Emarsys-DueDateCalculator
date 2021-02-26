2be15cab077e26aca1ac4ac67eb8ab539629df23
Takacs Robert <89.t.robert@gmail.com>
2021-02-26 10:06:02 +0100

    feat(): added webpack config files

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: [
+                    '/node_modules',
+                    '/src/tests'
+                ],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const common = require('./webpack.common');
+const path = require('path');
+const { merge } = require('webpack-merge');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'development',
+    watch: true,
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new NodemonPlugin({
+            script: './server.js',
+            watch: path.resolve('./dist')
+        })
+    ]
+});
